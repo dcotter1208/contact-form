5b3f968670195a8c638370655fd19fe4f3715391
Donovan <donovan.cotter@detroitlabs.com>
2020-12-21 10:13:03 -0500

    Added contact form with basic name inputs

---
+++ src/form/ContactForm.styles.ts
@@ -0,0 +1,22 @@
+import { createStyles, makeStyles, Theme } from "@material-ui/core";
+
+let styles = (theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+      flexDirection: "column",
+      width: "60%",
+      margin: "10px auto auto auto",
+    },
+    textInput: {
+      margin: "10px 0",
+    },
+    title: {
+      alignSelf: "flex-start",
+      fontWeight: "bold",
+    },
+  });
+
+const useStyles = makeStyles(styles, { index: 1 });
+
+export default useStyles;
---
+++ src/form/ContactForm.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { TextField } from "@material-ui/core";
+import useStyles from "./ContactForm.styles";
+import Header from "../landing/header";
+
+function ContactForm() {
+  const classes = useStyles();
+
+  return (
+    <>
+      <form className={classes.root} noValidate autoComplete='off'>
+        <Header
+          className={classes.title}
+          variant={"h5"}
+          title={"Contact Info"}
+        />
+        <TextField
+          className={classes.textInput}
+          id='first name'
+          label='First Name'
+          variant='outlined'
+        />
+        <TextField
+          className={classes.textInput}
+          id='first name'
+          label='Last Name'
+          variant='outlined'
+        />
+        <TextField
+          className={classes.textInput}
+          id='first name'
+          label='First Name'
+          variant='outlined'
+        />
+        <TextField
+          className={classes.textInput}
+          id='first name'
+          label='First Name'
+          variant='outlined'
+        />
+      </form>
+    </>
+  );
+}
+
+export default ContactForm;
